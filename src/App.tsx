e6e0c3a9042fe9357c96d34270da5d6858c0803d
Jahan <Jahanulhaque@gmail.com>
2025-12-28 12:57:38 +0000

    remove unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+// import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/Home/Home';
 // import Login from './pages/Login/Login';
 // import BookingPage from './pages/Booking/BookingPage';
@@ -8,16 +8,16 @@ import './App.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/book" element={<BookingPage />} /> */}
-          <Route path="/prices" element={<PricesPage />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-        </Routes>
-      </Router>
-    </AuthProvider>
+    // <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/book" element={<BookingPage />} /> */}
+        <Route path="/prices" element={<PricesPage />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
+    // </AuthProvider>
   );
 }
 
